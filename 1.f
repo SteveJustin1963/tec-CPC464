1cdab8ff1def874ae6b4a5f396b2ce1277b9c6e9
Stephen Justin <stevejustin1963@gmail.com>
2023-06-28 13:35:13 +1000

    Create 1.f

---
+++ 1.f
@@ -0,0 +1,68 @@
+The code provided is written in the Arduino variant of C++, which is a static, compiled language. In contrast, Forth is a stack-based, interactive language, and is not typically used for embedded hardware programming.
+
+However, converting some key aspects of this code into Forth could look something like this:
+
+```forth
+2 constant G
+3 constant FORCE_READY
+4 constant IS_WRITE
+
+8 constant D0
+9 constant D1
+10 constant D2
+11 constant D3
+12 constant D4
+13 constant D5
+6 constant D6
+7 constant D7
+
+variable buf 256 allot
+
+: setup
+  9600 serial-begin
+  G pin-input
+  IS_WRITE pin-input
+  FORCE_READY pin-output
+  LOW FORCE_READY digital-write
+  OUTPUT busMode
+  ." Ready" cr ;
+
+: busMode ( mode -- )
+  DUP D0 pin-mode
+  DUP D1 pin-mode
+  DUP D2 pin-mode
+  DUP D3 pin-mode
+  DUP D4 pin-mode
+  DUP D5 pin-mode
+  DUP D6 pin-mode
+  D7 pin-mode ;
+
+255 variable b
+
+: loop
+  G digital-read IF EXIT THEN
+  IS_WRITE digital-read IF
+    ." Write was requested" cr
+    INPUT busMode
+    PIND B11000000 AND PINB OR b !
+    b @ . ." Received" cr
+    HIGH FORCE_READY digital-write
+    G digital-read 0= UNTIL
+    LOW FORCE_READY digital-write
+  ELSE
+    ." Read was requested" cr
+    OUTPUT busMode
+    b @ B00111111 AND PORTB OR!
+    b @ B11000000 AND PORTD OR!
+    HIGH FORCE_READY digital-write
+    G digital-read 0= UNTIL
+    LOW FORCE_READY digital-write
+    INPUT busMode
+  THEN ;
+```
+
+This is an attempt at converting some aspects of the original Arduino code into a version of Forth. It's important to note that the hardware interactions (`pin-input`, `pin-output`, `digital-write`, `digital-read`, etc.) are pseudo-operations, as Forth doesn't usually interact with hardware in this way directly. You would need to provide appropriate definitions for these words based on the specific Forth system and hardware platform you're using. Furthermore, the Forth language doesn't usually use polling loops as used in Arduino loop functions, and it's usually better to organize the program in a different way.
+
+Also, the complex part of the code, involving direct manipulation of ports (`PORTB` and `PORTD`), has been left as is, because it would be hardware-dependent and would need a specific Forth system with appropriate facilities for port I/O. Forth isn't typically used to do the low-level hardware access in this manner, although it can be if the Forth system has appropriate primitives and the hardware platform permits it.
+
+So while this code should give you a sense of how you might write similar code in Forth, it won't actually run on a Forth system without additional definitions and modifications to suit the Forth system and the hardware platform.
